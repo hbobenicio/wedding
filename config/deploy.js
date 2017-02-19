958498c2065d2ec85fd4bc6b6380cd203c0f93d8
Hugo Benício <hbobenicio@gmail.com>
2017-02-19 16:20:50 -0300

    Adding ember-cli-deploy addon and fixing layouts

---
+++ config/deploy.js
@@ -0,0 +1,47 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    // include other plugin configuration that applies to all deploy targets here
+    'revision-data': {
+      type: 'git-commit'
+    },
+
+    'ssh-index': {
+      remoteDir: "/home/hugo/deploys/wedding",
+      username: "hugo",
+      host: "45.55.225.67",
+      privateKeyFile: "/home/hugo/.ssh/id_rsa",
+      allowOverwrite: true,
+      passphrase: 'aluufc98'
+    },
+
+    rsync: {
+      dest: "/home/hugo/deploys/wedding",
+      username: "hugo",
+      host: "45.55.225.67",
+      delete: false
+    }
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
