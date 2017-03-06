4dce62a17d767bf7f4569f132e97e6359cb0b87e
Wedding <wedding@hugobenicio.com.br>
2017-03-06 01:30:09 -0300

    Configuring brocolli-asset-rev to working on apache

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,20 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    minifyJS: {
+      enabled: false
+    },
+    minifyCSS: {
+      enabled: false
+    },
+
     // Add options here
+    fingerprint: {
+      exclude: ['vendor.css', 'vendor.js', 'wedding.css', 'wedding.js'],
+      ignore: ['index.html'],
+      prepend: 'http://www.hugobenicio.com.br/wedding/'
+//      enabled: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
