9babace867794c5e4c2d1562c7a05e2e43dbe1e8
Hugo Benício <hbobenicio@gmail.com>
2017-03-06 03:04:32 -0300

    Documenting fingerprint configuration options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,17 +11,68 @@ module.exports = function(defaults) {
       enabled: false
     },
 
-    // Add options here
+    /**
+     * Fingerprinting is done using the addon broccoli-asset-rev (which is included by default).
+     *
+     * When the environment is production (e.g. ember build --environment=production),
+     * the addon will automatically fingerprint your js, css, png, jpg, and gif assets
+     * by appending an md5 checksum to the end of their filename
+     * (e.g. assets/yourapp-9c2cbd818d09a4a742406c6cb8219b3b.js).
+     * 
+     * In addition, your html, js, and css files will be re-written to include the new name.
+     * There are a few options you can pass in to EmberApp in your ember-cli-build.js to customize this behavior.
+     */
     fingerprint: {
-      // Adding SVG to the default extensions
+      /**
+       * Default: app.env === 'production' - Boolean.
+       * Enables fingerprinting if true. True by default if current environment is production.
+       */
+      //enabled: app.env === 'production',
+
+      /**
+       * Default: [] - An array of strings.
+       * If a filename contains any item in the exclude array, it will not be fingerprinted.
+       * 
+       * Excluding vendor and wedding js and css (to work with production apache)
+       */
+      exclude: ['vendor.css', 'vendor.js', 'wedding.css', 'wedding.js'],
+
+      /**
+       * Default: [] - An array of strings.
+       * If a filename contains any item in the ignore array,
+       * the contents of the file will not be processed for fingerprinting.
+       */
+      // ignore: [],
+
+      /**
+       * Default: ['js', 'css', 'png', 'jpg', 'gif', 'map']
+       * The file types to add md5 checksums.
+       * 
+       * Adding SVG to the default extensions
+       */
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg'],
 
-      // Excluding vendor and wedding js and css (to work with production apache)
-      exclude: ['vendor.css', 'vendor.js', 'wedding.css', 'wedding.js'],
     
-      // Prepending sources url's to match production base path
+      /**
+       * Default: '' - A string to prepend to all of the assets.
+       * Useful for CDN urls like https://subdomain.cloudfront.net/
+       * 
+       * Prepending the production prefix path to the url's of the sources so that it the correct base path.
+       */
       prepend: 'http://www.hugobenicio.com.br/wedding/'
-    }
+
+      /**
+       * Default: ['html', 'css', 'js'] - The file types to replace source code with new checksum file names.
+       */
+      //replaceExtensions: ['html', 'css', 'js']
+
+      /**
+       * When specified, this is appended to fingerprinted filenames instead of the md5.
+       * Pass null to suppress the hash, which can be useful when using prepend.
+       */
+      //customHash: {...}
+
+    } // fingerprint
   });
 
   // Use `app.import` to add additional libraries to the generated
