585a7e19acb674fce244589c427b3c9fbb6525d9
Hugo Benício <hbobenicio@gmail.com>
2017-03-13 08:21:08 -0300

    A lot of refactoring

--- tests/integration/components/page-menu-test.js
+++ tests/integration/components/page-menu-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('page-jumbotron', 'Integration | Component | page jumbotron', {
+moduleForComponent('page-menu', 'Integration | Component | page menu', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{page-jumbotron}}`);
+  this.render(hbs`{{page-menu}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#page-jumbotron}}
+    {{#page-menu}}
       template block text
-    {{/page-jumbotron}}
+    {{/page-menu}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
