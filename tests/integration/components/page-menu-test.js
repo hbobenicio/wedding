8afb997dbb30a3d82497ab99a5632af066efd653
Hugo Benício <hbobenicio@gmail.com>
2017-07-06 20:58:51 -0300

    Corrigindo os tests de integração

--- tests/integration/components/page-menu-test.js
+++ tests/integration/components/page-menu-test.js
@@ -13,13 +13,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{page-menu}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#page-menu}}
-      template block text
-    {{/page-menu}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
